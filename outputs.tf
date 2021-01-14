9935dd531166745037a478155e3b4184112e4f17
Warren Uniewski <warren@autimo.com>
2021-01-13 16:41:50 -0800

    Initial commit

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
-//module outputs should be defined and documented here.
+# outputs.tf
+
+output "alb_hostname" {
+  value = aws_alb.main.dns_name
+}
+
+output "sns_topic" {
+  value       = aws_sns_topic.billing_alert_topic.arn
+  description = "Subscribe to this topic using your email to receive email alerts from the budget."
+}
