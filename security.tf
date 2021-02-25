18a53d22409968394b4019fa91ec4bf15f4e7392
Warren Uniewski <warren@autimo.com>
2021-02-24 17:20:38 -0800

    remove endpoints security group

--- security.tf
+++ security.tf
@@ -52,27 +52,3 @@ resource "aws_security_group" "ecs_tasks" {
 
   tags = local.common_tags
 }
-
-
-
-resource "aws_security_group" "endpoints" {
-  name        = "sample-endpoints-security-group"
-  description = "allow inbound access"
-  vpc_id      = module.network.aws_vpc.id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = local.common_tags
-}
