1f28cb1baa40c45f4c2e9a48657a54294f43ff9e
Prabhu-Manchineella_bcgovent <prabhu.manchineella@gov.bc.ca>
2023-06-19 12:12:21 -0400

    remove s3 bucket acl's

--- s3.tf
+++ s3.tf
@@ -7,9 +7,3 @@ resource "aws_s3_bucket" "upload_bucket" {
   bucket        = random_pet.upload_bucket_name.id
   force_destroy = true
 }
-
-resource "aws_s3_bucket_acl" "upload_bucket_acl" {
-  depends_on = [aws_s3_bucket.upload_bucket]
-  bucket = aws_s3_bucket.upload_bucket.id
-  acl    = "private"
-}
