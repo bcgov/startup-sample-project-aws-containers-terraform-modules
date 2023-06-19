193e974121c8cc0d23698ee13084734bd82975fe
Prabhu-Manchineella_bcgovent <prabhu.manchineella@gov.bc.ca>
2023-06-19 11:59:00 -0400

    Update deprecated resources for s3.tf

--- s3.tf
+++ s3.tf
@@ -5,6 +5,11 @@ resource "random_pet" "upload_bucket_name" {
 
 resource "aws_s3_bucket" "upload_bucket" {
   bucket        = random_pet.upload_bucket_name.id
-  acl           = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "upload_bucket_acl" {
+  depends_on = [aws_s3_bucket.upload_bucket]
+  bucket = aws_s3_bucket.upload_bucket.id
+  acl    = "private"
 }
